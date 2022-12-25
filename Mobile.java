98fcbd77d660d9b4da3987bfc3fe5386c58042ae
Jayanthi-git <121364367+Jayanthi-git@users.noreply.github.com>
2022-12-25 14:32:36 +0000

    Add files via upload
    day1Assignment

--- Mobile.java
+++ Mobile.java
@@ -3,10 +3,18 @@ package week1.day1Assignment;
 public class Mobile {
 public void sendMsg() {
 	System.out.println("This is my mobile");
+	boolean isFullyCharged= false;
+	System.out.println(isFullyCharged);
+	int mobileCost = 45000;
+	System.out.println(mobileCost); 
 	System.out.println("Hi I am sending Message");
 }
 public void makeCall() {
+	String mobileModel ="Samsung";
+	float mobileWeight =2.8f;
 	System.out.println("I am making call");
+	System.out.println(mobileModel);
+	System.out.println(mobileWeight);
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
